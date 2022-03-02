a459003ed56066a0e03d35cff19cd99f618ba0e9
Yuva Teja <yuvateja.kyama@gmail.com>
2022-03-02 22:21:02 +0530

    favorateMovie_2

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -1,14 +1,14 @@
 const movies = [];
 
 function favouriteMovie(operation, movie) {
-    // Write your code here
-    if (operation === 'add'){
-        movies.push(movie)
+    // Write your code here 
+    if (operation === "add"){ 
+        movies.push(movie)  //[inception,lucy]
     }
-    if (operation === 'remove'){
+    if (operation === "remove"){
         movies.pop()
     }
-    return movies
+    return movies //inception
 }
 
 module.exports = favouriteMovie;
