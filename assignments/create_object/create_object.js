33c5b20a989f28c0c5b346822b1f735092f994f7
Yuva Teja <yuvateja.kyama@gmail.com>
2022-03-05 23:52:43 +0530

    createObject

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,16 @@
 function CreateObject(arr) {
     // Write your code here
+    let object = {};
+    let value = [];
+    let key = [];
+    for (let i=0;i<arr.length;i++){
+        if (i%2 === 0){
+            key.push(arr[i]);
+        }else{
+            value.push(arr[i]);
+        }
+    }
+    key.forEach((key,i)=>object[key]=value[i])
+    return object
 }
-
 module.exports = CreateObject;
