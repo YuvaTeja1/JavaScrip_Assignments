3d0a4e1cd760c918228b7131984bfd74bf8a4971
Yuva Teja <yuvateja.kyama@gmail.com>
2022-03-10 22:34:48 +0530

    todoList

---
+++ assignments/todo_list/script.js
@@ -0,0 +1,29 @@
+let count=0;
+
+document.getElementsByTagName("button")[0].addEventListener('click',function(){
+    let data=document.getElementsByTagName("input")[0].value;
+    if(data=="")
+{
+    alert("Empty")
+}
+else{
+    let div_data=document.getElementsByClassName("todos")[0];
+    
+    let p=document.createElement("p");
+    p.setAttribute('key',count);
+    //let key=0;
+    count+=1;
+    p.textContent=document.getElementsByTagName("input")[0].value;
+    div_data.appendChild(p);
+    document.getElementsByTagName("input")[0].value="";
+
+    p.addEventListener('click',function(){
+        div_data.removeChild(p);
+    
+    })
+    
+    
+}
+
+
+})
